dd68e9aea0d19cbfa4de9f824c56804b462857e2
Saim20 <saimulislam2002@gmail.com>
2024-03-16 04:05:05 +0600

    Fixed expenses list

--- lib/pages/expenses/expenses_list.dart
+++ lib/pages/expenses/expenses_list.dart
@@ -17,159 +17,189 @@ class _ExpenseListPageState extends State<ExpenseListPage> {
 
   bool getData = false;
 
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-
-    //TODO Fix list
-
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Filter(
-              initialDate: startDate,
-              label: 'Start Date',
-              onChanged: (DateTime selectedDate) {
-                startDate = selectedDate;
-              },
-            ),
-            Filter(
-              initialDate: endDate,
-              label: 'End Date',
-              onChanged: (DateTime selectedDate) {
-                endDate = selectedDate;
-              },
-            ),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ElevatedButton(
-            onPressed: () {
-              setState(() {
-                getData = true;
-              });
-            },
-            child: const Text('Get Data'),
+    return Form(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Filter(
+                startDate: startDate,
+                label: 'Start Date',
+                onChanged: (DateTime selectedDate) {
+                  startDate = selectedDate;
+                  if (startDate.isAfter(endDate)) {
+                    setState(() {
+                      endDate = startDate;
+                    });
+                  }
+                },
+              ),
+              Filter(
+                startDate: endDate,
+                label: 'End Date',
+                onChanged: (DateTime selectedDate) {
+                  endDate = selectedDate;
+                  if (endDate.isBefore(startDate)) {
+                    setState(() {
+                      startDate = endDate;
+                    });
+                  }
+                },
+              ),
+            ],
           ),
-        ),
-        if (getData)
-          FutureBuilder(
-            future: FirebaseFirestore.instance
-                .collection('expenses')
-                .where('date', isGreaterThanOrEqualTo: startDate)
-                .where('date', isLessThanOrEqualTo: endDate)
-                .get(), // Function to fetch data from Firebase
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              } else if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              } else {
-                num totalAmount = 0;
-                for (var i = 0; i < snapshot.data!.docs.length; i++) {
-                  totalAmount += snapshot.data!.docs[i]['cashAmount'];
-                }
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  getData = true;
+                });
+              },
+              child: const Text('Get Data'),
+            ),
+          ),
+          if (getData)
+            FutureBuilder(
+              future: FirebaseFirestore.instance
+                  .collection('expenses')
+                  .where('transactionDate', isGreaterThanOrEqualTo: startDate)
+                  .where('transactionDate', isLessThanOrEqualTo: endDate)
+                  .get(), // Function to fetch data from Firebase
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  num totalAmount = 0;
+                  for (var i = 0; i < snapshot.data!.docs.length; i++) {
+                    totalAmount += snapshot.data!.docs[i]['amount'];
+                  }
 
-                return Expanded(
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
+                  return Expanded(
                     child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: DataTable(
-                          border: TableBorder.all(
-                            color: Colors.blueGrey,
-                            width: 2,
-                            borderRadius: BorderRadius.circular(6),
+                      scrollDirection: Axis.vertical,
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: DataTable(
+                            border: TableBorder.all(
+                              color: Colors.blueGrey,
+                              width: 2,
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            headingTextStyle: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                            ),
+                            // headingRowColor:
+                            //     MaterialStateProperty.all(Colors.blueGrey),
+                            columns: const <DataColumn>[
+                              DataColumn(
+                                label: Text('Transaction Date'),
+                              ),
+                              DataColumn(
+                                label: Text('Incurred Date'),
+                              ),
+                              DataColumn(
+                                label: Text('Expense Item'),
+                              ),
+                              DataColumn(
+                                label: Text('Payment Method'),
+                              ),
+                              DataColumn(
+                                label: Text('Amount'),
+                              ),
+                            ],
+                            rows: [
+                              ...snapshot.data!.docs.map<DataRow>(
+                                (DocumentSnapshot document) {
+                                  return DataRow(
+                                    cells: <DataCell>[
+                                      DataCell(
+                                        Text(
+                                          formattedDate(
+                                            (document['transactionDate']
+                                                    as Timestamp)
+                                                .toDate(),
+                                          ),
+                                        ),
+                                      ),
+                                      DataCell(
+                                        Text(
+                                          formattedDate(
+                                            (document['incurredDate']
+                                                    as Timestamp)
+                                                .toDate(),
+                                          ).substring(3),
+                                        ),
+                                      ),
+                                      DataCell(
+                                        Text(
+                                          document['expenseItem'],
+                                        ),
+                                      ),
+                                      DataCell(
+                                        Text(
+                                          document['paymentMethod'],
+                                        ),
+                                      ),
+                                      DataCell(
+                                        Text(
+                                          document['amount'].toString(),
+                                        ),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              ),
+                              DataRow(
+                                cells: [
+                                  const DataCell(
+                                    Text(
+                                      '',
+                                    ),
+                                  ),
+                                  const DataCell(
+                                    Text(
+                                      '',
+                                    ),
+                                  ),
+                                  const DataCell(
+                                    Text(
+                                      '',
+                                    ),
+                                  ),
+                                  const DataCell(
+                                    Text(
+                                      'Total:',
+                                    ),
+                                  ),
+                                  DataCell(
+                                    Text(
+                                      totalAmount.toString(),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
-                          headingTextStyle: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 17,
-                          ),
-                          // headingRowColor:
-                          //     MaterialStateProperty.all(Colors.blueGrey),
-                          columns: const <DataColumn>[
-                            DataColumn(
-                              label: Text('Date'),
-                            ),
-                            DataColumn(
-                              label: Text('Expense Item'),
-                            ),
-                            DataColumn(
-                              label: Text('Payment Method'),
-                            ),
-                            DataColumn(
-                              label: Text('Amount'),
-                            ),
-                          ],
-                          rows: [
-                            ...snapshot.data!.docs.map<DataRow>(
-                              (DocumentSnapshot document) {
-                                return DataRow(
-                                  cells: <DataCell>[
-                                    DataCell(
-                                      Text(
-                                        formattedDate(
-                                            (document['date'] as Timestamp)
-                                                .toDate()),
-                                      ),
-                                    ),
-                                    DataCell(
-                                      Text(
-                                        document['expenseItem'],
-                                      ),
-                                    ),
-                                    DataCell(
-                                      Text(
-                                        document['paymentMethod'],
-                                      ),
-                                    ),
-                                    DataCell(
-                                      Text(
-                                        document['cashAmount'].toString(),
-                                      ),
-                                    ),
-                                  ],
-                                );
-                              },
-                            ),
-                            DataRow(
-                              cells: [
-                                const DataCell(
-                                  Text(
-                                    '',
-                                  ),
-                                ),
-                                const DataCell(
-                                  Text(
-                                    '',
-                                  ),
-                                ),
-                                const DataCell(
-                                  Text(
-                                    'Total:',
-                                  ),
-                                ),
-                                DataCell(
-                                  Text(
-                                    totalAmount.toString(),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
                         ),
                       ),
                     ),
-                  ),
-                );
-              }
-            },
-          ),
-      ],
+                  );
+                }
+              },
+            ),
+        ],
+      ),
     );
   }
 }
@@ -178,12 +208,12 @@ class Filter extends StatefulWidget {
   const Filter({
     super.key,
     required this.onChanged,
-    required this.initialDate,
+    required this.startDate,
     required this.label,
   });
 
   final Function(DateTime picked) onChanged;
-  final DateTime initialDate;
+  final DateTime startDate;
   final String label;
 
   @override
@@ -196,7 +226,7 @@ class _FilterState extends State<Filter> {
   @override
   void initState() {
     super.initState();
-    _transactionDate = widget.initialDate;
+    _transactionDate = widget.startDate;
   }
 
   @override
